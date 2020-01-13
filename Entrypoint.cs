b5411dd265539cd3e51b03fe350a286e2bd1ec66
Rea <1171053+ReaTsukuda@users.noreply.github.com>
2020-01-13 12:57:28 -0800

    initial VS files

---
+++ Entrypoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EO3EquipmentEdit
+{
+  static class Entrypoint
+  {
+    [STAThread]
+    static void Main()
+    {
+      Application.EnableVisualStyles();
+      Application.Run(new MainForm());
+    }
+  }
+}
